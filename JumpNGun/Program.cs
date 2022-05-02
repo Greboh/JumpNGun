5e6fffae7bea24227d30b44221e6a26c1316c7ec
Keanoski <41839506+Keanoski@users.noreply.github.com>
2022-05-02 13:42:41 +0200

    MonoGame init

---
+++ JumpNGun/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JumpNGun
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
