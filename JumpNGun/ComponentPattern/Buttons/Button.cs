fb5630ee7a45653ebfe593cd760deeaff31b43f5
Keanoski <41839506+Keanoski@users.noreply.github.com>
2022-05-23 22:25:25 +0200

    Menu repositioning

--- JumpNGun/ComponentPattern/Buttons/Button.cs
+++ JumpNGun/ComponentPattern/Buttons/Button.cs
@@ -12,6 +12,12 @@ namespace JumpNGun
     {
         private Vector2 _position;
 
+        //hardcoded button positions
+        private Vector2 _startButtonPosition = new Vector2(566, 365);
+        private Vector2 _settingButtonPosition = new Vector2(517, 437);
+        private Vector2 _highscoreButtonPosition = new Vector2(470, 505);
+        private Vector2 _quitButtonPosition = new Vector2(577, 575);
+
         private ButtonType _type;
 
         private Rectangle _mouseRect;
@@ -29,19 +35,21 @@ namespace JumpNGun
 
         public override void Awake()
         {
+
+            // Sets position for the button sprite
             switch (_type)
             {
                 case ButtonType.Start:
-                    _position = new Vector2(GameWorld.Instance.ScreenSize.X / 2.5f, 365);
+                    _position = _startButtonPosition;
                     break;
                 case ButtonType.Settings:
-                    _position = new Vector2(GameWorld.Instance.ScreenSize.X / 2.5f, 437);
+                    _position = _settingButtonPosition;
                     break;
                 case ButtonType.Highscores:
-                    _position = new Vector2(GameWorld.Instance.ScreenSize.X / 2.5f, 525);
+                    _position = _highscoreButtonPosition;
                     break;
                 case ButtonType.Quit:
-                    _position = new Vector2(GameWorld.Instance.ScreenSize.X / 2.5f, 590);
+                    _position = _quitButtonPosition;
                     break;
             }
         }
@@ -50,30 +58,20 @@ namespace JumpNGun
         {
             GameObject.Transform.Position = _position;
             _sr = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
-            _sr.SetOrigin(Vector2.Zero);
-            _buttonRect = new Rectangle((int) _position.X, (int) _position.Y, _sr.Sprite.Width, _sr.Sprite.Height);
-
-            switch (_type)
-            {
-                case ButtonType.Start:
-                    break;
-                case ButtonType.Settings:
-                    break;
-                case ButtonType.Highscores:
-                    break;
-                case ButtonType.Quit:
-                    break;
-            }
+            _sr.SetOrigin(Vector2.Zero); // sets origin to top left
+            _buttonRect = new Rectangle((int) _position.X, (int) _position.Y, _sr.Sprite.Width, _sr.Sprite.Height); // construcs rectangle from position and sprite width/height
         }
 
         public override void Update(GameTime gameTime)
         {
-            _mouseRect = new Rectangle((int) GameWorld.Instance.myMouse.X, (int) GameWorld.Instance.myMouse.Y, 50, 50);
+            _mouseRect = new Rectangle((int) GameWorld.Instance.myMouse.X, (int) GameWorld.Instance.myMouse.Y, 10, 10); // mouse rectangle
 
+            // used for checking mouse rect, sprite rect intersection
             if (_mouseRect.Intersects(_buttonRect))
             {
-                _sr.SetColor(Color.Aqua);
+                _sr.SetColor(Color.LightGray);
 
+                
                 switch (_type)
                 {
                     case ButtonType.Start:
@@ -101,7 +99,17 @@ namespace JumpNGun
 
                         break;
                     case ButtonType.Quit:
+                        if (GameWorld.Instance.myMouse.LeftButton == ButtonState.Pressed && _canIntersect)
+                        {
 
+                            GameWorld.Instance.Exit();
+
+                            _canIntersect = false;
+                        }
+                        else if (GameWorld.Instance.myMouse.LeftButton == ButtonState.Released && !_canIntersect)
+                        {
+                            _canIntersect = true;
+                        }
                         break;
                 }
             }
